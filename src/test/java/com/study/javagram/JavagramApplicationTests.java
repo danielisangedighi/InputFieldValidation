65ef604cef3376768ea19e7692c804d5b542effe
danielisangedighi <danielisangedighi@outlook.com>
2024-05-13 07:04:55 +0100

    initial commit after dependencies

---
+++ src/test/java/com/study/javagram/JavagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.javagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
